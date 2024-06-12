cce493586b6ddd5dd9347769c8f28cccca90391a
20Boris1998 <70031598@continental.edu.pe>
2024-06-12 14:28:36 -0500

    TDD C#

---
+++ PromedioCalculatorApp/PromedioCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace PromedioCalculatorApp
+{
+    public class PromedioCalculator
+    {
+        public double CalcularPromedio(int[] conjunto)
+        {
+            if (conjunto == null || conjunto.Length == 0)
+                throw new ArgumentException("El conjunto no puede estar vacío o ser nulo.");
+
+            return conjunto.Average();
+        }
+    }
+}
---
+++ PromedioCalculatorTests/PromedioCalculatorTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+using PromedioCalculatorApp;
+
+namespace PromedioCalculatorTests
+{
+    public class PromedioCalculatorTests
+    {
+        [Fact]
+        public void CalcularPromedio_ConjuntoValido_RetornaPromedioCorrecto()
+        {
+            // Arrange
+            var calculator = new PromedioCalculator();
+            int[] conjunto = { 1, 2, 3, 4, 5 };
+
+            // Act
+            double resultado = calculator.CalcularPromedio(conjunto);
+
+            // Assert
+            Assert.Equal(3.0, resultado);
+        }
+
+        [Fact]
+        public void CalcularPromedio_ConjuntoVacio_LanzaArgumentException()
+        {
+            // Arrange
+            var calculator = new PromedioCalculator();
+            int[] conjunto = { };
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => calculator.CalcularPromedio(conjunto));
+        }
+
+        [Fact]
+        public void CalcularPromedio_ConjuntoNulo_LanzaArgumentException()
+        {
+            // Arrange
+            var calculator = new PromedioCalculator();
+            int[] conjunto = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => calculator.CalcularPromedio(conjunto));
+        }
+
+        [Fact]
+        public void CalcularPromedio_ConjuntoConUnElemento_RetornaElElemento()
+        {
+            // Arrange
+            var calculator = new PromedioCalculator();
+            int[] conjunto = { 42 };
+
+            // Act
+            double resultado = calculator.CalcularPromedio(conjunto);
+
+            // Assert
+            Assert.Equal(42.0, resultado);
+        }
+
+        [Fact]
+        public void CalcularPromedio_ConjuntoConNumerosNegativos_RetornaPromedioCorrecto()
+        {
+            // Arrange
+            var calculator = new PromedioCalculator();
+            int[] conjunto = { -1, -2, -3, -4, -5 };
+
+            // Act
+            double resultado = calculator.CalcularPromedio(conjunto);
+
+            // Assert
+            Assert.Equal(-3.0, resultado);
+        }
+    }
+}
